b5607dd3271737f3fc89c9c50a86ae39b6f74bd7
dikshadhote <39937404+dikshadhote@users.noreply.github.com>
2021-01-20 21:30:21 +0530

    processing

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
+//button click
 var btnTranslate=document.querySelector("#btn-translate");
+//take input from text area
 var textareaInput=document.querySelector("#textarea-input");
+//show op
 var outputArea=document.querySelector("#output-area");
 
 
